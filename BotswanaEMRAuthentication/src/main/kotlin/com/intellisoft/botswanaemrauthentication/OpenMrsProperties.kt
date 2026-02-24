4d804778ffa57460da00e18395951d457b50ffab
Dave <davidnjau21@gmail.com>
2026-02-24 20:45:24 +0300

    Updated the the Retrofit configuration

--- BotswanaEMRAuthentication/src/main/kotlin/com/intellisoft/botswanaemrauthentication/OpenMrsProperties.kt
+++ BotswanaEMRAuthentication/src/main/kotlin/com/intellisoft/botswanaemrauthentication/OpenMrsProperties.kt
@@ -1,4 +1,12 @@
 package com.intellisoft.botswanaemrauthentication
 
-class OpenMrsProperties {
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationProperties(prefix = "openmrs")
+open class OpenMrsProperties {
+    lateinit var username: String
+    lateinit var password: String
+    lateinit var url: String
 }
